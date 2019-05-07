dc17ee3d9c3194ece878538fdd214c91362a2ea7
Anders Bærø <andersbaero@gmail.com>
2019-05-07 14:51:59 +0200

    Implemented serializable in Turneringer

--- admapp/src/klasser/Turnering.java
+++ admapp/src/klasser/Turnering.java
@@ -6,42 +6,42 @@
 package klasser;
 
 import java.io.File;
+import java.io.Serializable;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 /**
  *
  * @author andersbaero
  */
-public class Turnering {
+public class Turnering implements Serializable{
     private final String navn;
     private final String fraDato;
     private final String tilDato;
     private final String sted;
     private final String fil;
 
-    public Turnering(String navn, String fraDato, String tilDato, String sted) {
-        this.navn = navn;
-        this.fraDato = fraDato;
-        this.tilDato = tilDato;
-        this.sted = sted;
-        this.fil = "../turneringer/"+navn;
-        //Eks /turneringer/bøsjakkmesterskap
-    }
-    
-    
-    //lag mappe
-    //
-    //sjekk om mappe allerede eksisterer
-    
-    public void lagMappe(){
-        if(!mappeFinnes()){
-            new File(this.fil).mkdirs();
+    public Turnering(String navn, String fraDato, String tilDato, String sted){
+        if(!mappeFinnes("/turneringer"+navn)){
+            this.navn = navn;
+            this.fraDato = fraDato;
+            this.tilDato = tilDato;
+            this.sted = sted;
+            this.fil = "/turneringer/"+navn;
+            lagMappe();
         }else{
-            System.out.println("Mappe finnes");
+            showMessageDialog(null, "Turnering eksisterer allerede!");
+            throw new IllegalArgumentException("Turnering eksiterer allerede!");
         }
     }
     
-    public boolean mappeFinnes(){
-        if (new File(this.fil).exists()){
+    
+    public void lagMappe(){    
+        new File(this.fil).mkdirs();
+        
+    }
+    
+    public boolean mappeFinnes(String innFil){
+        if(new File(innFil).exists()){
             return true;
         }else{
             return false;
