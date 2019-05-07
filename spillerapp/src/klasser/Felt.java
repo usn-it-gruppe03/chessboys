cf9114bd553895c59965547dd4ddf5130f47bce5
Isak Hauge <isakhauge@gmail.com>
2019-05-07 13:26:22 +0200

    Add klasse-folder

--- spillerapp/src/klasser/Felt.java
+++ spillerapp/src/klasser/Felt.java
@@ -2,9 +2,31 @@ package klasser;
 
 import javafx.scene.shape.Rectangle;
 
+
+/**
+ * Klasse: Felt
+ * */
 public class Felt extends Rectangle {
 
-    public Felt(){
+    Posisjon posisjon;
+
+    /**
+     * Konstruktør
+     * */
+    public Felt(Posisjon posisjon, double x, double y, double bredde, double høyde){
+        super();
+        this.posisjon = posisjon;
+        this.setX(x);
+        this.setY(y);
+        this.setWidth(bredde);
+        this.setHeight(høyde);
+    }
+
+    public getSenterX(){
+
+    }
+
+    public setSenterX(double x){
 
     }
 
