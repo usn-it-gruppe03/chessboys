f216dc84f7270aa3a02fbf5d55f07348583dc547
Anders Bærø <andersbaero@gmail.com>
2019-05-07 13:32:51 +0200

    Add getters to Turnering

--- admapp/src/klasser/Turnering.java
+++ admapp/src/klasser/Turnering.java
@@ -10,11 +10,11 @@ package klasser;
  * @author andersbaero
  */
 public class Turnering {
-    private String navn;
-    private String fraDato;
-    private String tilDato;
-    private String sted;
-    private String fil;
+    private final String navn;
+    private final String fraDato;
+    private final String tilDato;
+    private final String sted;
+    private final String fil;
 
     public Turnering(String navn, String fraDato, String tilDato, String sted) {
         this.navn = navn;
@@ -29,6 +29,26 @@ public class Turnering {
     //lag mappe
     //new File("/path/directory").mkdirs();
     //sjekk om mappe allerede eksisterer
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public String getFraDato() {
+        return fraDato;
+    }
+
+    public String getTilDato() {
+        return tilDato;
+    }
+
+    public String getSted() {
+        return sted;
+    }
+
+    public String getFil() {
+        return fil;
+    }
     
     
 }
